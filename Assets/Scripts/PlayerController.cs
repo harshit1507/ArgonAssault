5f5f472330b7575fd0d9bf2b53faac4e8cc06ec5
harshit1507 <harshit1507@yahoo.com>
2021-05-12 23:18:00 +0530

    Move Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
+    //[SerializeField] InputAction movement;
+
+    [SerializeField] float controlSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void OnEnable()
-    {
-        movement.Enable();
-    }
+    //private void OnEnable()
+    //{
+    //    movement.Enable();
+    //}
 
-    private void OnDisable()
-    {
-        movement.Disable();
-    }
+    //private void OnDisable()
+    //{
+    //    movement.Disable();
+    //}
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        //float horizontalThrow = movement.ReadValue<Vector2>().x;
+        //float verticalThrow = movement.ReadValue<Vector2>().y;
 
 
-        //float horizontalThrow = Input.GetAxis("Horizontal");
-        //float verticalThrow = Input.GetAxis("Vertical");
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
 
-        Debug.Log(horizontalThrow);
-        Debug.Log(verticalThrow);
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newyPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(newXPos, newyPos, transform.localPosition.z);
     }
 }
