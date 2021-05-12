90d291eb3e19e6fb860943b8298eed826445d1d6
harshit1507 <harshit1507@yahoo.com>
2021-05-12 22:39:34 +0530

    Take input from InputSystem(New Method)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        //float verticalThrow = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
     }
 }
