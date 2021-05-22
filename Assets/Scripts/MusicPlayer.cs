ebd16bc4c0672ae6bc6e46e60e8e3ca18ab1953e
harshit1507 <harshit1507@yahoo.com>
2021-05-22 17:13:20 +0530

    add music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        int count = FindObjectsOfType<MusicPlayer>().Length;
+        if(count > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
